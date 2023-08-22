227b48c96089376df3b7046d364542b14e058dfd
ThGoulis <95432036+ThGoulis@users.noreply.github.com>
2023-08-22 17:41:04 +0300

    Add files via upload

--- BaselineHtml/main.py
+++ BaselineHtml/main.py
@@ -71,76 +71,10 @@ graphs = {
     '44.swEcologicalStatusOrPotentialExpectedGoodIn2015.csv': eeafunctions._44_swEcologicalStatusOrPotentialExpectedGoodIn2015,
     '45.swEcologicalStatusOrPotentialExpectedAchievementDate2016.csv': eeafunctions._45_swEcologicalStatusOrPotentialExpectedAchievementDate2016,
     '46.swChemicalStatusExpectedGoodIn2015.csv': eeafunctions._46_swChemicalStatusExpectedGoodIn2015,
-    '47.swChemicalStatusExpectedAchievementDate2016.csv': eeafunctions._47_swChemicalStatusExpectedAchievementDate2016,
-
-    # '4.SOW_SWB_SWB_swSignificantImpactType_Table_Overall2016.csv': eeafunctions._4_SOW_SWB_SWB_swSignificantImpactType_Table_Overall2016
-    # '4.SOW_SWB_SWB_swSignificant_Pressure_Type_Table_Overall2016.csv': eeafunctions._4_SOW_SWB_SWB_swSignificant_Pressure_Type_Table_Overall2016,
-    # '9.swEcologicalStatusOrPotential_All_Category2ndRBMP2016.csv': eeafunctions._9_swEcologicalStatusOrPotential_All_Category2ndRBMP2016,
-    # '11.swEcologicalStatusOrPotential_by_Category_Total_By_Category2ndRBMP2016.csv': eeafunctions._11_swEcologicalStatusOrPotential_by_Category_Total_By_Category2ndRBMP2016,
-    # '11.swEcologicalStatusOrPotential_by_Country_Total_Status2ndRBMP.csv': eeafunctions._11_swEcologicalStatusOrPotential_by_Country_Total_Status2ndRBMP,
-    # '12.swChemicalStatusValue_by_Country2016.csv': eeafunctions._12_swChemicalStatusValue_by_Country2016,
-    # '13.gwChemicalStatus_Table2016.csv': eeafunctions._13_gwChemicalStatus_Table2016,
-    # '14.swEcologicalStatusOrPotential_by_Category_Total_Status2ndRBMP2016.csv': eeafunctions._14_swEcologicalStatusOrPotential_by_Category_Total_Status2ndRBMP2016,
-    # '15.swEcologicalStatusOrPotentialValue_by_Country2016.csv': eeafunctions._15_swEcologicalStatusOrPotentialValue_by_Country2016,
-    # '15.swEcologicalStatusOrPotential_ChemicalStatus_by_Category2016.csv': eeafunctions._15_swEcologicalStatusOrPotential_ChemicalStatus_by_Category2016,
-    # '15.swEcologicalStatusOrPotential_ChemicalStatus_by_Category_Chemical2016.csv': eeafunctions._15_swEcologicalStatusOrPotential_ChemicalStatus_by_Category_Chemical2016
-    # '15.swEcologicalStatusOrPotential_ChemicalStatus_by_Category_Ecological2016.csv': eeafunctions._15_swEcologicalStatusOrPotential_ChemicalStatus_by_Category_Ecological2016,
-    # '19.GroundWaterBodyCategoryQuantitative_status2016.csv': eeafunctions._19_GroundWaterBodyCategoryQuantitative_status2016
-    # '40.swPollutants.csv': eeafunctions._40_swPollutants,
-    # '40.swRBsPollutantsOther.csv': eeafunctions._40_swRBsPollutantsOther,
-
-    # New Dashboards
-    # 'NewDash.5.swPrioritySubstanceCode2016.csv': eeafunctions.NewDash_5_swPrioritySubstanceCode2016,
-    # 'NewDash.6.surfaceWaterBodyTypeCode2016.csv': eeafunctions.NewDash_6_surfaceWaterBodyTypeCode2016,
-    # '7.swNumber_of_Impacts_by_country.csv': eeafunctions._7_swNumber_of_Impacts_by_country,
-    # 'NewDash.8.swNumber_of_impacts_by_country_by_category2016.csv': eeafunctions.NewDash_8_swNumber_of_impacts_by_country_by_category2016,
-    # '9.1.sw_delineation_of_the_management_units_in_the_1st_and_2nd_RBMP_Unchanged_2010.csv': eeafunctions._9_1_sw_delineation_of_the_management_units_in_the_1st_and_2nd_RBMP_Unchanged_2010,
-    # '9.1.sw_delineation_of_the_management_units_in_the_1st_and_2nd_RBMP_Unchanged_2016.csv': eeafunctions._9_1_sw_delineation_of_the_management_units_in_the_1st_and_2nd_RBMP_Unchanged_2016,
-    #
-    # # '9.2.sw_Evolution_type_by_Category_in_the_1st_and_2nd_RBMP_2016.csv': eeafunctions._9_2_sw_Evolution_type_by_Category_in_the_1st_and_2nd_RBMP_2016,
-    # '9.4.sw_Evolution_type_by_Country_and_Category_in_the_1st_and_2nd_RBMP2010.csv': eeafunctions._9_4_sw_Evolution_type_by_Country_and_Category_in_the_1st_and_2nd_RBMP2010,
-    # '9.4.sw_Evolution_type_by_Country_and_Category_in_the_1st_and_2nd_RBMP2016.csv': eeafunctions._9_4_sw_Evolution_type_by_Country_and_Category_in_the_1st_and_2nd_RBMP2016,
-    # '9.3.sw_Evolution_type_by_Country_in_the_1st_and_2nd_RBMP_2010.csv': eeafunctions._9_3_sw_Evolution_type_by_Country_in_the_1st_and_2nd_RBMP_2010,
-    # '9.3.sw_Evolution_type_by_Country_in_the_1st_and_2nd_RBMP_2016.csv': eeafunctions._9_3_sw_Evolution_type_by_Country_in_the_1st_and_2nd_RBMP_2016,
-    #
-    # '10.1.gw_delineation_of_the_management_units_in_the_1st_and_2nd_RBMP_Unchanged_Other_2010.csv': eeafunctions.gw_delineation_of_the_management_units_in_the_1st_and_2nd_RBMP_Unchanged_Other_2010,
-    # '10.1.gw_delineation_of_the_management_units_in_the_1st_and_2nd_RBMP_Unchanged_Other_2016.csv': eeafunctions.gw_delineation_of_the_management_units_in_the_1st_and_2nd_RBMP_Unchanged_Other_2016,
-    # '10.2.gw_Evolution_type_by_Country_in_the_1st_and_2nd_RBMP_2010.csv': eeafunctions._10_2_gw_Evolution_type_by_Country_in_the_1st_and_2nd_RBMP_2010,
-    #
-    # '10.2.gw_Evolution_type_by_Country_in_the_1st_and_2nd_RBMP_2016.csv': eeafunctions._10_2_gw_Evolution_type_by_Country_in_the_1st_and_2nd_RBMP_2016,
-    #
-    # 'NewDash.11.swEcologicalStatusOrPotential_by_Country2010.csv': eeafunctions.NewDash_11_swEcologicalStatusOrPotential_by_Country2010,
-    # 'NewDash.11.swEcologicalStatusOrPotential_by_Country2016.csv': eeafunctions.NewDash_11_swEcologicalStatusOrPotential_by_Country2016,
-    # 'NewDash.12.swChemical_by_Country2010.csv': eeafunctions.NewDash_12_swChemical_by_Country2010,
-    # 'NewDash.12.swChemical_by_Country2016.csv': eeafunctions.NewDash_12_swChemical_by_Country2016,
-    # 'NewDash.13.GroundWaterBodyCategoryQuantitative_status2010.csv': eeafunctions.NewDash_13_GroundWaterBodyCategoryQuantitative_status2010,
-    # 'NewDash.13.GroundWaterBodyCategoryQuantitative_status2016.csv': eeafunctions.NewDash_13_GroundWaterBodyCategoryQuantitative_status2016,
-    # 'NewDash.14.gwChemical_status_by_country2010.csv': eeafunctions.NewDash_14_gwChemical_status_by_country2010,
-    # 'NewDash.14.gwChemical_status_by_country2016.csv': eeafunctions.NewDash_14_gwChemical_status_by_country2016,
-    # # 'NewDash.18.Surface_water_bodies_QE1_status2016.csv': eeafunctions.NewDash_18_Surface_water_bodies_QE1_status2016,
-    # # 'NewDash.18.Surface_water_bodies_QE2_status2016.csv': eeafunctions.NewDash_18_Surface_water_bodies_QE2_status2016,
-    # # 'NewDash.18.Surface_water_bodies_QE3_status2016.csv': eeafunctions.NewDash_18_Surface_water_bodies_QE3_status2016,
-    # # 'NewDash.18.Surface_water_bodies_QE3_3_status2016.csv': eeafunctions.NewDash_18_Surface_water_bodies_QE3_3_status2016,
-    # 'NewDash.18.Surface_water_bodies_QE_status2016.csv': eeafunctions.NewDash_18_Surface_water_bodies_QE_status2016,
-    # 'NewDash.19.swEcological_status_or_potential_by_intercalibration_type_overall2016.csv': eeafunctions.NewDash_19_swEcological_status_or_potential_by_intercalibration_type_overall2016,
-    # 'NewDash.19.swEcological_status_or_potential_by_intercalibration_type2022.csv': eeafunctions.NewDash_19_swEcological_status_or_potential_by_intercalibration_type2022,
-    # 'NewDash.19.swEcological_status_or_potential_by_intercalibration_type_by_Category2022.csv': eeafunctions.NewDash_19_swEcological_status_or_potential_by_intercalibration_type_by_Category2022,
-    # 'NewDash.20.swEcologicalStatus_by_country2016.csv': eeafunctions.NewDash_20_swEcologicalStatus_by_country2016,
-    # 'NewDash.20.ChemicalStatus_Assessment_confidence_by_country2016.csv': eeafunctions.NewDash_20_ChemicalStatus_Assessment_confidence_by_country2016,
-    # 'NewDash.20.swEcologicalStatus_by_country_by_category2016.csv': eeafunctions.NewDash_20_swEcologicalStatus_by_country_by_category2016,
-    # # 'NewDash.20.swEcologicalStatus_by_country_by_category_overall2016.csv': eeafunctions.NewDash_20_swEcologicalStatus_by_country_by_category_overall2016,
-    # 'NewDash.20.ChemicalStatus_Assessment_confidence_by_country_by_category2016.csv': eeafunctions.NewDash_20_ChemicalStatus_Assessment_confidence_by_country_by_category2016,
-    # # 'NewDash.20.ChemicalStatus_Assessment_confidence_by_country_by_category_overall2016.csv': eeafunctions.NewDash_20_ChemicalStatus_Assessment_confidence_by_country_by_category_overall2016,
-    # 'NewDash.22.swPhysical_alterations_by_country2016.csv': eeafunctions.NewDash_22_swPhysical_alterations_by_country2016,
-    # 'NewDash.23.swDesignated_water_uses_by_country2016.csv': eeafunctions.NewDash_23_swDesignated_water_uses_by_country2016,
-    # 'NewDash.24.swEcologicalstatusorpotential_by_broadtype_by_country2016.csv': eeafunctions.NewDash_24_swEcologicalstatusorpotential_by_broadtype_by_country2016,
-    # '29.1.EcologicalMonitoring_Map_data.csv': eeafunctions._29_1_EcologicalMonitoring_Map_data,
-    # '29.2.ChemicalMonitoring_Map_data.csv': eeafunctions._29_2_ChemicalMonitoring_Map_data,
-    # '29.3.QuantitativeMonitoring_Map_data.csv': eeafunctions._29_3_QuantitativeMonitoring_Map_data,
+    '47.swChemicalStatusExpectedAchievementDate2016.csv': eeafunctions._47_swChemicalStatusExpectedAchievementDate2016
 }
 
-# countries = ['AT', 'BE', 'BG', 'CY', 'CZ', 'DE', 'DK', 'EE', 'EL', 'ES', 'FI', 'FR', 'HR', 'HU', 'IE', 'IT', 'LT', 'LU', 'LV',
-#             'MT', 'NL', 'NO', 'PL', 'PT', 'RO', 'SE', 'SI', 'SK', 'UK']
+
 
 
 for country in countries:
